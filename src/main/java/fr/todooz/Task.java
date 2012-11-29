cbffc663e696fb72b988b62ac3c17a01519e78f1
thomas BRIEN <tbrien@08-la-Pomme.local>
2012-11-29 12:04:34 +0100

    HelloWorldServlet fail

---
+++ src/main/java/fr/todooz/Task.java
@@ -0,0 +1,5 @@
+package fr.todooz;
+
+public class Task {
+
+}
---
+++ src/test/java/fr/todooz/http/HttpClientTest.java
@@ -0,0 +1,11 @@
+package fr.todooz.http;
+
+import org.junit.Test;
+
+public class HttpClientTest {
+
+	@Test
+	public void get() {
+
+	}
+}
